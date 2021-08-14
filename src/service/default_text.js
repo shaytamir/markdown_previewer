c704be6ff22f7e6452aa37492d2e1384151a5f87
shay <shay.lom.tamir@gmail.com>
2021-08-14 19:58:08 +0300

    'app'

--- src/service/default_text.js
+++ src/service/default_text.js
@@ -1,23 +1,3 @@
-import marked from "marked";
-
-// const renderer = {
-//   heading(text, level) {
-//     const escapedText = text.toLowerCase().replace(/[^\w]+/g, "-");
-
-//     return `
-//             <h${level}>
-//               <a name="${escapedText}" class="anchor" href="#${escapedText}">
-//                 <span class="header-link"></span>
-//               </a>
-//               ${text}
-//             </h${level}>`;
-//   },
-// };
-
-// marked.use({ renderer });
-const blockHtml = marked("**strong** _em_");
-const inlineHtml = marked.parseInline("**strong** _em_");
-
 export const defaultText = `
 # Welcome to shaysel's React Markdown Previewer!
 ## Shaysel's Editor
