601d42fbed8f91b9106138af4bca2b1b5863e08a
shay <shay.lom.tamir@gmail.com>
2021-08-14 19:51:46 +0300

    'app'

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import React, { useState, useEffect } from "react";
 
 import Preview from "./components/preview/preview";
 import Editor from "./components/textEditor/editor";
-// import "./previewer.css";
-// import marked from "marked";
-import Text from "./service/default_text.txt";
 import { defaultText } from "./service/default_text.js";
 
 function App() {
-  // const [defaultText, setDefaultText] = useState("Loading...");
   const [HTMLtext, setHTMLtext] = useState("loading.. ..");
   const [maximaized, setMaximaized] = useState(false);
 
   useEffect(() => {
     setHTMLtext(defaultText);
-    // fetch(Text)
-    //   .then((response) => response.text())
-    //   .then((textContent) => {
-    //   });
-    // return () => {
-    // }
   }, []);
 
   return (
@@ -36,8 +26,6 @@ function App() {
         }}
       />
       <Preview text={HTMLtext} max={maximaized} />
-      {/*  */}
-      {/*  */}
     </div>
   );
 }
